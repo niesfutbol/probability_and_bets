c681281f44e5b23a766e8c9aac2530e3f1f07142
Nepo Rojas <braulio.rojas@islas.org.mx>
2021-12-09 21:28:15 -0800

    👩🏿‍💻 Use real data

--- src/write_from_template.py
+++ src/write_from_template.py
@@ -1,50 +1,31 @@
+import pandas as pd
 from pydantic import BaseModel
 import json
 from jinja2 import Environment, FileSystemLoader
+
+def would_we_bet(match):
+    bet = (match["home"] > 0.5) | (match["away"] > 0.5)
+    return bet
+
+
+def add_color(to_render):
+    for match in to_render:
+        match.update({"color": "#218038"}) if would_we_bet(match) else match.update({"color": "#3aa0f3"})
+    return to_render
 
 class Match_Round(BaseModel):
     id_home: int
     id_away: int
     round: str
-
+path_1 = "tests/data/bets_39_2021_16.csv"
+path_2 = "tests/data/predictions_39_2021_16.csv"
+bets = pd.read_csv(path_1)
+predicionts = pd.read_csv(path_2)
+matches = bets.join(predicionts.set_index("id_match"), on = "id_match")
+to_render = [matches.iloc[i,:].to_dict() for i in range(len(matches))]
+to_render_with_color = add_color(to_render)
 fileLoader = FileSystemLoader("template")
 env = Environment(loader = fileLoader)
-matches = [{
-    "home_name": "Liverpool",
-    "home_id": 40,
-    "prob_home": "59 %",
-    "prob_away": "19 %",
-    "prob_draw": "12 %",
-    "bet_home": "1.90",
-    "bet_away": "3.90",
-    "bet_draw": "3.60",
-    "away_name": "Porto",
-    "away_id": 212,
-  },
-  {
-    "home_name": "Man City",
-    "home_id": 50,
-    "prob_home": "67 %",
-    "prob_away": "14 %",
-    "prob_draw": "19 %",
-    "bet_home": "1.60",
-    "bet_away": "4.33%",
-    "bet_draw": "5.00%",
-    "away_name": "PSG",
-    "away_id": 85,
-  },
-  {
-    "home_name": "Atl. Madrid",
-    "home_id": 530,
-    "prob_home": "67 %",
-    "prob_away": "14 %",
-    "prob_draw": "19 %",
-    "bet_home": "1.61",
-    "bet_away": "3.80",
-    "bet_draw": "5.50",
-    "away_name": "Milan",
-    "away_id": 489,
-  },]
 
-rendered = env.get_template("index.html").render(matches = matches)
+rendered = env.get_template("index.html").render(matches = to_render_with_color)
 print(rendered)
