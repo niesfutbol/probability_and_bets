7dd53ee178050795f66f6f331889c6fcc0dcae14
Nepo Rojas <braulio.rojas@islas.org.mx>
2021-12-12 16:43:09 -0800

    💩 Save buggy version

---
+++ nepomios/main.py
@@ -0,0 +1,39 @@
+import requests
+from jinja2 import Environment, FileSystemLoader
+
+from flask import Flask
+from flask import render_template
+
+def would_we_bet(match):
+    bet = (match["home"] > 0.5) | (match["away"] > 0.5)
+    return bet
+
+
+def add_color(to_render):
+    for match in to_render:
+        match.update({"color": "#218038"}) if would_we_bet(match) else match.update(
+            {"color": "#3aa0f3"}
+        )
+    return to_render
+
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'Index Page'
+
+@app.route('/hello')
+def hello():
+    conn = requests.get("https://rj2d6a.deta.dev")
+    res = conn.json()
+    to_render_with_color = add_color(to_render)
+    return 'Hello, World'
+
+@app.route('/user/<username>')
+def profile(username):
+    return f'{username}\'s profile'
+
+@app.route('/<league>')
+@app.route('/<league>')
