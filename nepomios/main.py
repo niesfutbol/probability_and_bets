44682e3140e6b81de9ad4f78e0585dba8deb30c3
Nepo Rojas <braulio.rojas@islas.org.mx>
2022-02-16 22:06:13 -0800

    🎨 Make format with black

--- nepomios/main.py
+++ nepomios/main.py
@@ -4,6 +4,7 @@ from jinja2 import Environment, FileSystemLoader
 from flask import Flask
 from flask import render_template
 
+
 def would_we_bet(match):
     bet = (match["home"] > 0.5) | (match["away"] > 0.5)
     return bet
@@ -17,26 +18,27 @@ def add_color(to_render):
     return to_render
 
 
-
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return render_template('choose_league.html')
 
-@app.route('/<league_season_round>')
+@app.route("/")
+def index():
+    return render_template("choose_league.html")
+
+
+@app.route("/<league_season_round>")
 def probability_and_bets(league_season_round):
     conn = requests.get(f"https://rj2d6a.deta.dev/{league_season_round}")
     res = conn.json()
     to_render = res["response"]
     to_render_with_color = add_color(to_render)
-    return render_template('index.html', matches=to_render_with_color)
+    return render_template("index.html", matches=to_render_with_color)
 
 
-@app.route('/liga/<country>')
+@app.route("/liga/<country>")
 def probability_and_bets_by_country(country):
     conn = requests.get(f"https://rj2d6a.deta.dev/liga/{country}")
     res = conn.json()
     to_render = res["response"]
     to_render_with_color = add_color(to_render)
-    return render_template('index.html', matches=to_render_with_color)
+    return render_template("index.html", matches=to_render_with_color)
